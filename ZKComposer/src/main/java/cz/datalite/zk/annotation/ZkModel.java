5493180cd9c03e70db1fdb41606ef54a11db1d6a
Jiri Bubnik <bubnik@datalite.cz>
2011-02-13 14:52:12 +0000

    ZkModel - minor language corrections
    git-svn-id: http://zk-dl.googlecode.com/svn/trunk@75 14f4ca54-e300-ebc0-3ca8-cadf33e97b03

--- ZKComposer/src/main/java/cz/datalite/zk/annotation/ZkModel.java
+++ ZKComposer/src/main/java/cz/datalite/zk/annotation/ZkModel.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  * the annotation. If getter or setter is created for this property, it is used
  * in getting or setting data. If getter or setter is not defined, value is
  * readed / setted directly from / to field even if it is private. Getter and
- * setter has to have has name like a pseudonym of this field. It means if name
+ * setter has to have the same name like a alias of this field. It means if name
  * is defined in this annotation then getter and setter has to have name like
  * definition in the annotation, no like a property name. Getter and setter
  * needn't be defined both.</p>
@@ -23,10 +23,10 @@ import java.lang.annotation.Target;
 public @interface ZkModel {
 
     /**
-     * Pseudonym of this property field. With this name filed will be
+     * Alias of this property field. With this name filed will be
      * accessible from zul also throw getters and setters. If it isn't
      * defined, property field name is used instead.
-     * @return  name property pseudonym
+     * @return  name property alias
      **/
     public String name() default "";
 }
