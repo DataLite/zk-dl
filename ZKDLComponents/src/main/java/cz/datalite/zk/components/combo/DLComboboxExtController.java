635e0e1d89f47d33e3026df8484b59d88ed2a311
Karel Cemus <karel.cemus@gmail.com>
2012-08-27 15:28:12 +0200

    ZK-167 Combobox deprecation
     - DLCombobox is not supported any more since ZK6 including

--- ZKDLComponents/src/main/java/cz/datalite/zk/components/combo/DLComboboxExtController.java
+++ ZKDLComponents/src/main/java/cz/datalite/zk/components/combo/DLComboboxExtController.java
@@ -8,7 +8,9 @@ import org.zkoss.zk.ui.util.Composer;
  * Extended controller for the combobox
  * @param <T> řídící entita controlleru
  * @author Karel Čemus <cemus@datalite.cz>
+ * @deprecated since 1.4.0, ZK 6, databinding 2. This component is not usable anymore
  */
+@Deprecated
 public interface DLComboboxExtController<T> extends Composer, DLComboboxController<T>, CascadableExt<T> {
 
     /**
