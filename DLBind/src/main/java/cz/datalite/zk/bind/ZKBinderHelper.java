45d8bfe2284645c0bb8f41a78a01a28cf8afb0a4
Karel Cemus <karel.cemus@gmail.com>
2012-08-27 15:27:43 +0200

    ZK-167 ZkBinderHelper simple access
     - created simple access to ZkBinderHelper methods with inner delegation to proper implementation
     - usage of the static methods is the shorten notation for getting the proper instance and calling desired method

--- DLBind/src/main/java/cz/datalite/zk/bind/ZKBinderHelper.java
+++ DLBind/src/main/java/cz/datalite/zk/bind/ZKBinderHelper.java
@@ -1,5 +1,8 @@
 package cz.datalite.zk.bind;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.zk.ui.AbstractComponent;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
 
@@ -10,6 +13,8 @@ import org.zkoss.zkplus.databind.AnnotateDataBinder;
  * @author Karel Cemus
  */
 public abstract class ZKBinderHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( ZKBinderHelper.class );
 
     private ZKBinderHelper() {
         // library class, facade
@@ -29,5 +34,73 @@ public abstract class ZKBinderHelper {
             return BinderHelperV2.INSTANCE;
 
         throw new UnsupportedOperationException( "Component has attached unsupported version of data binder." );
+    }
+
+    /** Returns wheather or not the component has attached any binder instance */
+    public static boolean hasBinder( Component component ) {
+        return component != null && component.getAttribute( "binder", true ) != null;
+    }
+
+    /** @see BinderHelper#loadComponent */
+    public static void loadComponent( Component comp ) {
+        if ( hasBinder( comp ) ) helper( comp ).loadComponent( comp );
+        else
+            LOGGER.warn( "Attempt to load a component but it has null or has not attached any binder." );
+    }
+
+    /** @see BinderHelper#loadComponentAttribute */
+    @Deprecated
+    public static void loadComponentAttribute( Component comp, String attribute ) {
+        if ( hasBinder( comp ) )
+            helper( comp ).loadComponentAttribute( comp, attribute );
+        else
+            LOGGER.warn( "Attempt to load a component's attribute but it has null or has not attached any binder." );
+    }
+
+    /** @see BinderHelper#saveComponent */
+    public static void saveComponent( Component comp ) {
+        if ( hasBinder( comp ) ) helper( comp ).saveComponent( comp );
+        else
+            LOGGER.warn( "Attempt to save a component but it has null or has not attached any binder." );
+    }
+
+    /** @see BinderHelper#saveComponentAttribute */
+    @Deprecated
+    public static void saveComponentAttribute( Component comp, String attribute ) {
+        if ( hasBinder( comp ) )
+            helper( comp ).saveComponentAttribute( comp, attribute );
+        else
+            LOGGER.warn( "Attempt to save a component's attribute but it has null or has not attached any binder." );
+    }
+
+    /** @see BinderHelper#loadComponentAttributes */
+    @Deprecated
+    public static void loadComponentAttributes( Component comp, String[] attributes ) {
+        if ( hasBinder( comp ) )
+            helper( comp ).loadComponentAttributes( comp, attributes );
+        else
+            LOGGER.warn( "Attempt to load a component's attributes but it has null or has not attached any binder." );
+    }
+
+    /**
+     * @param component Component with a registered binder to determine version of
+     * binding and also the component to be annotated
+     * @see BinderHelper#registerAnnotation */
+    public static void registerAnnotation( final AbstractComponent component, final String property, final String annotName, final String value ) {
+        if ( hasBinder( component ) )
+            helper( component ).registerAnnotation( component, property, annotName, value );
+        else
+            LOGGER.warn( "Attempt to load a component but it has null or has not attached any binder." );
+    }
+
+    /**
+     * @param comp Component with a registered binder to determine version of
+     * binding
+     * @see BinderHelper#notifyChange
+     */
+    public static void notifyChange( Component comp, Object bean, String model ) {
+        if ( hasBinder( comp ) ) helper( comp ).notifyChange( bean, model );
+        else
+            LOGGER.warn( "Attempt to load a component but it has null or has not attached any binder." );
     }
 }
