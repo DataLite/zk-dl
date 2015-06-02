4298f96118e48aed59faacbea4e37aa3618a51c7
Martin Peterka <martin.peterka@datalite.cz>
2015-06-02 12:17:39 +0200

    SPM-11 Vytvořit novou zk-dl 1.5.0 proti ZK8 Not allowed to set vflex and height at the same time except vflex="min"

--- DLBind/src/main/java/cz/datalite/zk/bind/Binder.java
+++ DLBind/src/main/java/cz/datalite/zk/bind/Binder.java
@@ -23,76 +23,89 @@ import org.zkoss.zkplus.databind.TypeConverter;
  */
 public class Binder extends AnnotateBinder {
 
-    /** instance of looger */
-    protected static final Logger LOGGER = LoggerFactory.getLogger( Binder.class );
+	/**
+	 * instance of looger
+	 */
+	protected static final Logger LOGGER = LoggerFactory.getLogger(Binder.class);
 
-    public boolean saveComponent( Component comp ) {
-        LOGGER.trace( "Saving the component '{}'.", comp.getClass() );
-        return saveAllBindings( comp );
-    }
+	public Binder() {
+		super();
+	}
 
-    public void loadComponent( Component comp ) {
-        LOGGER.trace( "Loading the component '{}'.", comp.getClass() );
-        super.loadComponent( comp, false );
-    }
 
-    @Deprecated
-    public void loadComponentAttribute( Component comp, String attribute ) {
-        LOGGER.trace( "Loading the attribute '{}' on the component '{}'.", attribute, comp.getClass() );
-        loadBinding( comp, new BindingKey( comp, attribute ) );
-    }
+	public Binder(String qname, String qscope) {
+		super(qname, qscope);
+	}
 
-    @Deprecated
-    public boolean saveComponentAttribute( Component comp, String attribute ) {
-        LOGGER.trace( "Saving the attribute '{}' on the component '{}'.", attribute, comp.getClass() );
-        return saveBinding( comp, new BindingKey( comp, attribute ) );
-    }
+	public static Binder getBinder(Component comp) {
+		return (Binder) comp.getAttribute("binder", true);
+	}
 
-    public static Binder getBinder( Component comp ) {
-        return ( Binder ) comp.getAttribute( "binder", true );
-    }
+	public boolean saveComponent(Component comp) {
+		LOGGER.trace("Saving the component '{}'.", comp.getClass());
+		return saveAllBindings(comp);
+	}
 
-    @Override
-    public Converter getConverter( String name ) {
-        try {
-            return super.getConverter( name );
-        } catch ( ClassCastException ex ) {
-            // converter found but class doesn't meet requirements
-            // try to find adapter
-            return resolveConverterAdapter( name );
-        } catch ( UiException ex ) {
-            // this allows to use method converters
-            // syntax is then like 'ctl.coerceToValue'
-            return new MethodConverter( name, ex.getMessage() );
-        }
-    }
+	public void loadComponent(Component comp) {
+		LOGGER.trace("Loading the component '{}'.", comp.getClass());
+		super.loadComponent(comp, false);
+	}
 
-    /** Support for TypeConverter and MethodConverter */
-    private Converter resolveConverterAdapter( String name ) {
-        try {
+	@Deprecated
+	public void loadComponentAttribute(Component comp, String attribute) {
+		LOGGER.trace("Loading the attribute '{}' on the component '{}'.", attribute, comp.getClass());
+		loadBinding(comp, new BindingKey(comp, attribute));
+	}
+
+	@Deprecated
+	public boolean saveComponentAttribute(Component comp, String attribute) {
+		LOGGER.trace("Saving the attribute '{}' on the component '{}'.", attribute, comp.getClass());
+		return saveBinding(comp, new BindingKey(comp, attribute));
+	}
+
+	@Override
+	public Converter getConverter(String name) {
+		try {
+			return super.getConverter(name);
+		} catch (ClassCastException ex) {
+			// converter found but class doesn't meet requirements
+			// try to find adapter
+			return resolveConverterAdapter(name);
+		} catch (UiException ex) {
+			// this allows to use method converters
+			// syntax is then like 'ctl.coerceToValue'
+			return new MethodConverter(name, ex.getMessage());
+		}
+	}
+
+	/**
+	 * Support for TypeConverter and MethodConverter
+	 */
+	private Converter resolveConverterAdapter(String name) {
+		try {
 //            Class converter = Classes.forNameByThread( name ); // XXX v ZK8 neni?
-            Class converter = Class.forName( name );
-            if ( TypeConverter.class.isAssignableFrom( converter ) ) {
-                LOGGER.debug( "Converter '{}' is not directly support in ZK 6 and later. You should consider conversion to 'org.zkoss.bind.Converter'.", converter.getClass() );
-                return new TypeConverterAdapter( ( TypeConverter ) converter.newInstance() );
-            } else
-                throw new ClassNotFoundException( "Convertor has to implement 'Converter' or 'TypeConverter' interface." );
-        } catch ( InstantiationException | IllegalAccessException ex ) {
-            LOGGER.error( "Converter adapter couldn't be created.", ex );
-            throw new UiException( ex );
-        } catch ( ClassNotFoundException ex ) {
-            throw new UiException( ex );
-        }
-    }
+			Class converter = Class.forName(name);
+			if (TypeConverter.class.isAssignableFrom(converter)) {
+				LOGGER.debug("Converter '{}' is not directly support in ZK 6 and later. You should consider conversion to 'org.zkoss.bind.Converter'.", converter.getClass());
+				return new TypeConverterAdapter((TypeConverter) converter.newInstance());
+			} else
+				throw new ClassNotFoundException("Convertor has to implement 'Converter' or 'TypeConverter' interface.");
+		} catch (InstantiationException | IllegalAccessException ex) {
+			LOGGER.error("Converter adapter couldn't be created.", ex);
+			throw new UiException(ex);
+		} catch (ClassNotFoundException ex) {
+			throw new UiException(ex);
+		}
+	}
 
-    @Override
-    public Validator getValidator( String name ) {
-        try {
-            return super.getValidator( name );
-        } catch ( org.zkoss.zk.ui.UiException ex ) {
-            // this allows to use method converters
-            // syntax is then like 'ctl.coerceToValue'
-            return new MethodValidator( name, ex.getMessage() );
-        }
-    }
+	@Override
+	public Validator getValidator(String name) {
+		try {
+			return super.getValidator(name);
+		} catch (org.zkoss.zk.ui.UiException ex) {
+			// this allows to use method converters
+			// syntax is then like 'ctl.coerceToValue'
+			return new MethodValidator(name, ex.getMessage());
+		}
+	}
 }
--- ZKComposer/src/main/java/cz/datalite/zk/bind/AnnotationBinder.java
+++ ZKComposer/src/main/java/cz/datalite/zk/bind/AnnotationBinder.java
@@ -4,12 +4,13 @@ import cz.datalite.zk.annotation.invoke.CommandContext;
 import cz.datalite.zk.annotation.invoke.Invoke;
 import cz.datalite.zk.annotation.processor.AnnotationProcessor;
 import cz.datalite.zk.annotation.processor.InvokeListener;
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Set;
 import org.zkoss.bind.BindContext;
 import org.zkoss.bind.Property;
 import org.zkoss.zk.ui.Component;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Binder extension providing support ZK-DL for annotation processing
@@ -18,50 +19,61 @@ import org.zkoss.zk.ui.Component;
  */
 public class AnnotationBinder extends Binder {
 
-    @Override
-    protected void doExecute( Component comp, String command, Map<String, Object> commandArgs, BindContext ctx, Set<Property> notifys ) {
-        // class of view-model instance
-        final Class type = getViewModel().getClass();
-        // get the annotation processor of the view-model instance
-        final AnnotationProcessor processor = AnnotationProcessor.getProcessor( type );
+	public AnnotationBinder() {
+		super();
+	}
 
-        // annotated method to be invoked
-        final Method method = getCommandMethod( type, command );
-        if ( method == null ) {
-            LOGGER.debug( "Binder caught request for '{}' command but ZK BinderImpl doesn't know the mapping method. Execution failed.", command );
-            return;
-        }
 
-        // get command invoker wrapped by the ZK-DL annotation invokers
-        final Invoke invoker = processor.getCommandInvoker( method );
-        if ( invoker == null ) {
-            // report error and finish
-            LOGGER.error( "Binder tries to invoke the command invoker on the method '{}' but the invoker wasn't found.", method.getName() );
-            return;
-        }
+	public AnnotationBinder(String qname, String qscope) {
+		super(qname, qscope);
+	}
 
-        // invoke the command invoker
-        LOGGER.trace( "Binder invokes the command '{}' on the method '{}'.", command, method.getName() );
+	@Override
+	protected void doExecute(Component comp, String command, Map<String, Object> commandArgs, BindContext ctx, Set<Property> notifys) {
+		// class of view-model instance
+		final Class type = getViewModel().getClass();
+		// get the annotation processor of the view-model instance
+		final AnnotationProcessor processor = AnnotationProcessor.getProcessor(type);
 
-        // initialize the context
-        final CommandContext context = new CommandContext( invoker, getViewModel(), getView() );
-        context.init( command, commandArgs, ctx, notifys, comp );
+		// annotated method to be invoked
+		final Method method = getCommandMethod(type, command);
+		if (method == null) {
+			LOGGER.debug("Binder caught request for '{}' command but ZK BinderImpl doesn't know the mapping method. Execution failed.", command);
+			return;
+		}
 
-        try {
-            // invoke the command
-            InvokeListener.invoke( context );
-        } catch ( RuntimeException ex ) {
-            // rethrow runtime exception
-            // to let them be properly handled by ZK
-            throw ex;
-        } catch ( Exception ex ) {
-            // something has happened, log it
-            LOGGER.error( "Binder invoked command '{}' on the method '{}' but it failed.", new Object[]{ command, method.getName(), ex } );
-        }
-    }
+		// get command invoker wrapped by the ZK-DL annotation invokers
+		final Invoke invoker = processor.getCommandInvoker(method);
+		if (invoker == null) {
+			// report error and finish
+			LOGGER.error("Binder tries to invoke the command invoker on the method '{}' but the invoker wasn't found.", method.getName());
+			return;
+		}
 
-    /** Resumes the execution of ZKOSS @Command */
-    public void resumeCommand( Component comp, String command, Map<String, Object> commandArgs, BindContext ctx, Set<Property> notifys ) {
-        super.doExecute( comp, command, commandArgs, ctx, notifys );
-    }
+		// invoke the command invoker
+		LOGGER.trace("Binder invokes the command '{}' on the method '{}'.", command, method.getName());
+
+		// initialize the context
+		final CommandContext context = new CommandContext(invoker, getViewModel(), getView());
+		context.init(command, commandArgs, ctx, notifys, comp);
+
+		try {
+			// invoke the command
+			InvokeListener.invoke(context);
+		} catch (RuntimeException ex) {
+			// rethrow runtime exception
+			// to let them be properly handled by ZK
+			throw ex;
+		} catch (Exception ex) {
+			// something has happened, log it
+			LOGGER.error("Binder invoked command '{}' on the method '{}' but it failed.", new Object[]{command, method.getName(), ex});
+		}
+	}
+
+	/**
+	 * Resumes the execution of ZKOSS @Command
+	 */
+	public void resumeCommand(Component comp, String command, Map<String, Object> commandArgs, BindContext ctx, Set<Property> notifys) {
+		super.doExecute(comp, command, commandArgs, ctx, notifys);
+	}
 }
