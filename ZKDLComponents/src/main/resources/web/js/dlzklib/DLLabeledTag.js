f4ffec8d7a389b7a4e2d66900fd1053c3dc2c9ff
Martin Peterka <martin.peterka@datalite.cz>
2015-06-17 08:09:56 +0200

    SPM-11 Vytvořit novou zk-dl 1.5.0 proti ZK8 + DLLabeledTag component + PagingPageSize component

---
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/DLLabeledTag.js
@@ -0,0 +1,34 @@
+dlzklib.DLLabeledTag = zk.$extends(zul.Widget, {
+	_label: "",
+	_value: "",
+
+   $define: {
+       label: function(label) {
+		   if (this.desktop) {
+			  this.$n("label").innerHTML = zUtl.encodeXML(label);
+		   }
+       },
+       value: function(value) {
+		   if (this.desktop) {
+			   this.$n("value").innerHTML = zUtl.encodeXML(value);
+		   }
+       }
+    },
+
+    bind_: function () {
+        this.$supers(dlzklib.DLLabeledTag, 'bind_', arguments);
+        this.domListen_(this.$n("close"), "onClick", "doRemove");
+    },
+
+    unbind_: function () {
+        this.domUnlisten_(this.$n("close"), "onClick", "doRemove");
+        this.$supers(dlzklib.DLLabeledTag, 'unbind_', arguments);
+    },
+
+    doRemove: function(evt) {
+        this.fire("onRemove", this.getValue(), {});
+        var $el = jq(this);
+        $el.hide('fast', function(){ $el.remove(); });
+    }
+
+});
---
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/css/DLLabeledTag.css.dsp
@@ -0,0 +1,34 @@
+<%@ taglib uri="http://www.zkoss.org/dsp/web/core" prefix="c" %>
+div.z-dllabeledtag {
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    display: block;
+    float: left;
+    margin: 0px 2px 4px 0;
+    position: relative;
+    border: 1px solid #7eaac6;
+    display: inline-block;
+    text-decoration: none;
+    padding: 2px 7px;
+    font-size: 12px;
+    line-height: 12px;
+    vertical-align: middle;
+    border-radius: 4px;
+    /*box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.2), inset 0 1px 0 0 rgba(255, 255, 255, 0.4);*/
+    background: #f0faff;
+}
+
+div.z-dllabeledtag span.dl-labeled-tag-value {
+    font-style: italic;
+    margin-left: 0.7em;
+}
+
+div.z-dllabeledtag span.dl-labeled-tag-close {
+    padding-left: 0.5em;
+}
+
+div.z-dllabeledtag span.dl-labeled-tag-close:hover {
+    color: #000;
+    text-shadow: 1px 1px 4px #4b99ed;
+    cursor: pointer;
+}
