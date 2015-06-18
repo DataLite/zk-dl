37ee30bc705da474d92f82578c859054586a67e2
Jan Tecl <tecl@datalite.cz>
2015-06-18 09:03:32 +0200

    Oprava názvu souboru

---
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
@@ -0,0 +1,8 @@
+(function (out) {
+    var uuid = this.uuid;
+    out.push('<div ', this.domAttrs_(), ' id="', uuid , '-root">');
+    out.push('<span class="dl-labeled-tag-label" id="', uuid, '-label">', this.getLabel(), '</span>');
+    out.push('<span class="dl-labeled-tag-value" id="', uuid, '-value">', this.getValue(), '</span>');
+    out.push('<span class="dl-labeled-tag-close" id="', uuid, '-close">×</span>');
+    out.push('</div>');
+});
