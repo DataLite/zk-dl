23387fdad987b1305d865bea945264ee92d7beab
Jan Tecl <tecl@datalite.cz>
2015-07-15 01:27:23 +0200

    SPM-10 Úpravy pro použití se ZK 8

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
@@ -3,6 +3,6 @@
     out.push('<div ', this.domAttrs_(), ' id="', uuid , '-root">');
     out.push('<span class="dl-labeled-tag-label" id="', uuid, '-label">', this.getLabel(), '</span>');
     out.push('<span class="dl-labeled-tag-value" id="', uuid, '-value">', this.getValue(), '</span>');
-    out.push('<span class="z-icon-times" id="', uuid, '-close"></span>');
+    out.push('<span class="dl-labeled-tag-close" id="', uuid, '-close">×</span>');
     out.push('</div>');
 });
