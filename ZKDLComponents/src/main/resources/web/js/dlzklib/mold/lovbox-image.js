001c42ecc24aa2c5dc56d159d11710014dbef976
Jan Tecl <tecl@datalite.cz>
2016-02-09 23:39:16 +0100

    SPM-10 úprava javascriptu generujího lovbox v případě, že je nastaven mold image

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/lovbox-image.js
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/lovbox-image.js
@@ -1,9 +1,11 @@
 (function (out) {
+
     var uuid = this.uuid,
         zcls = this.getZclass();
 
     out.push('<i', this.domAttrs_(), '><input id="',
         uuid, '-real" style="display:none"/>');
+    out.push('<i class="z-btn z-button">');
 
     if (this.getImageLabel()) {
         out.push('<div id="', uuid, '-label-btn" class="', zcls, '-label-btn z-button-dtl">');
@@ -13,7 +15,7 @@
         out.push('</div>');
 
     } else {
-        out.push('<i id="', uuid, '-btn" class="', zcls, '-image-btn"></i>');
+        out.push('<i id="', uuid, '-btn" class="z-icon-three-bars"></i>');
     }
 
 
@@ -21,5 +23,6 @@
 
     this.redrawpp_(out);
 
+    out.push('</i>');
     out.push('</i>');
 })
