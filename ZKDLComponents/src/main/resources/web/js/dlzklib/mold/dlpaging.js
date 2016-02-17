4ccbc9b76caff54718eab5e1bc9792647fa21316
Jan Tecl <tecl@datalite.cz>
2016-02-17 22:37:34 +0100

    SPM-10 doplnění aux-content componenty dlpaging, zvýšení verze na 1.5.0.1

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dlpaging.js
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dlpaging.js
@@ -1,28 +1,33 @@
 
 function (out) {
-	if (this.getMold() == "os") {
-		out.push('<div', this.domAttrs_(), '>', this._innerTags(), '</div>');
-		return;
-	}
-	
-	var uuid = this.uuid,
-		btn = this.$s('button');
-	
-	out.push('<div name="', uuid, '"', this.domAttrs_(), '><ul>',
-			'<li><a name="', uuid, '-first" class="', btn, ' ', this.$s('first'),
-				'" href="javascript:;"><i class="z-paging-icon z-icon-angle-double-left"></i></a></li>',
-			'<li><a name="', uuid, '-prev" class="', btn, ' ', this.$s('previous'),
-				'" href="javascript:;"><i class="z-paging-icon z-icon-angle-left"></i></a></li>',
-			'<li><input name="',
-				uuid, '-real" class="', this.$s('input'), '" type="text" value="',
-				this.getActivePage() + 1, '" size="3"></input><span class="',
-				this.$s('text'), '"> / ', this.getPageCount(), '</span></li>',
-			'<li><a name="', uuid, '-next" class="', btn, ' ', this.$s('next'),
-				'" href="javascript:;"><i class="z-paging-icon z-icon-angle-right"></i></a></li>',
-			'<li><a name="', uuid, '-last" class="', btn, ' ', this.$s('last'),
-				'" href="javascript:;"><i class="z-paging-icon z-icon-angle-double-right"></i></a></li></ul>');
-	
-	if (this.isDetailed())
-		this._infoTags(out);
-	out.push('</div>');
+    if (this.getMold() == "os") {
+        out.push('<div', this.domAttrs_(), '>', this._innerTags(), '</div>');
+        return;
+    }
+
+    var uuid = this.uuid,
+        btn = this.$s('button');
+
+    out.push('<div name="', uuid, '"', this.domAttrs_(), '><ul>',
+        '<li><a name="', uuid, '-first" class="', btn, ' ', this.$s('first'),
+        '" href="javascript:;"><i class="z-paging-icon z-icon-angle-double-left"></i></a></li>',
+        '<li><a name="', uuid, '-prev" class="', btn, ' ', this.$s('previous'),
+        '" href="javascript:;"><i class="z-paging-icon z-icon-angle-left"></i></a></li>',
+        '<li><input name="',
+        uuid, '-real" class="', this.$s('input'), '" type="text" value="',
+        this.getActivePage() + 1, '" size="3"></input><span class="',
+        this.$s('text'), '"> / ', this.getPageCount(), '</span></li>',
+        '<li><a name="', uuid, '-next" class="', btn, ' ', this.$s('next'),
+        '" href="javascript:;"><i class="z-paging-icon z-icon-angle-right"></i></a></li>',
+        '<li><a name="', uuid, '-last" class="', btn, ' ', this.$s('last'),
+        '" href="javascript:;"><i class="z-paging-icon z-icon-angle-double-right"></i></a></li></ul>');
+
+    // any children
+    out.push('<div class="z-dlpaging-aux-content">');
+    this.redrawChildren(out);
+    out.push('</div>');
+
+    if (this.isDetailed())
+        this._infoTags(out);
+    out.push('</div>');
 }
