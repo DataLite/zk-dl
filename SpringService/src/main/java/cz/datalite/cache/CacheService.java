266fc60e11781e783437b73e4b31199252e5e090
Ondrej Milenovsky <milenovsky@datalite.cz>
2017-02-27 10:46:20 +0100

    CacheService: merged updates from master

--- SpringService/src/main/java/cz/datalite/cache/CacheService.java
+++ SpringService/src/main/java/cz/datalite/cache/CacheService.java
@@ -1,7 +1,11 @@
 package cz.datalite.cache;
 
-
 import cz.datalite.cache.model.ServiceResult;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Mezipamět pro ukládání vytvořených objektů
@@ -55,10 +59,23 @@ public interface CacheService
 
     /**
      * @param cacheType     typ pole
+     * @return položka existuje
+     */
+    <CacheType> boolean isExistsInCache(Class<CacheType> cacheType);
+
+    /**
+     * @param cacheType     typ pole
      * @param key           hledaný klíč
      * @return hodnota z cache
      */
     <CacheType, XmlType, DatabaseType> DatabaseType getValueFromCache(Class<CacheType> cacheType, XmlType key) ;
+
+    /**
+     * @param regExpClassName     regularni vyraz pro určení plného názvu třídy
+     * @param key                 hledaný klíč
+     * @return hodnota z cache
+     */
+    <XmlType, DatabaseType> DatabaseType getValueFromCache( String regExpClassName, XmlType key) ;
 
     /**
      * Vymazání mezipaměti
@@ -72,4 +89,24 @@ public interface CacheService
      * @param value         hodnota
      */
     <XmlType> void addServiceResultToCache( XmlType key, ServiceResult value ) ;
+
+    /**
+     * Vrati vsechny objekty danych trid.
+     * @param cacheTypes pozadovane tridy
+     * @return seznam objektu danych trid
+     */
+    List<?> getAllValues(Collection<Class<?>> cacheTypes);
+
+    /**
+     * Vrati mapu objektu dane tridy.
+     * @param cacheType trida objektu
+     * @return immutable mapa klic->objekt
+     */
+    <CacheType> Map getAllValues(Class<CacheType> cacheType);
+
+    /**
+     * Vrati vchny tridy v cachi.
+     * @return immutable mnozina trid objektu z cache
+     */
+    Set<Class<?>> getAllClasses();
 }
