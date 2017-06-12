4534850baea988994374ab916854cc41fa8c4143
Jan Tecl <tecl@datalite.cz>
2017-06-12 16:49:37 +0200

    SPM-10 stylování/úpravy - projekt, diagramy, rozšířený filtr

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
@@ -1,6 +1,6 @@
 (function (out) {
     var uuid = this.uuid;
-    out.push('<div ', this.domAttrs_(), ' id="', uuid , '-root">');
+    out.push('<div draggable="true" droppable="true" ', this.domAttrs_(), ' id="', uuid , '-root">');
     out.push('<span class="dl-labeled-tag-label" id="', uuid, '-label">', this.getLabel(), '</span>');
     out.push('<span class="dl-labeled-tag-value" id="', uuid, '-value">', this.getValue(), '</span>');
     out.push('<span class="dl-labeled-tag-close" id="', uuid, '-close">×</span>');
