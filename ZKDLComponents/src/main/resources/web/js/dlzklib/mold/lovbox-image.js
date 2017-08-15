e49ba024f78ac82664c5d9ba80fe837f380c7d09
Vojtěch Kárný <karny@datalite.cz>
2017-08-15 11:12:49 +0200

    Odstranění nastavení třídy disd v lovbox pokud je jako tlačítko

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/lovbox-image.js
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/lovbox-image.js
@@ -7,16 +7,10 @@
 
     // noinspection JSUnresolvedFunction
     if (this.getImageLabel()) {
-        var disabled="" ;
-        // noinspection JSUnresolvedFunction
-        if ( ( this.getLovboxReadonly() ) || ( this.getDisabled() ) ) {
-            disabled="-disd" ;
-        }
-        out.push('<div id="', uuid, '-label-btn" class="', zcls, '-label-btn z-button-dtl', disabled, '">');
-        // noinspection JSUnresolvedFunction
-        out.push('<span class="fl">',this.getImageLabel(),'</span>');
-
-        out.push('<i id="', uuid, '-btn" class="', zcls, '-image-btn"></i>');
+        out.push('<div id="', uuid, '-label-btn" class="', zcls, '-label-btn z-button-dtl">');
+            // noinspection JSUnresolvedFunction
+            out.push('<span class="fl">',this.getImageLabel(),'</span>');
+            out.push('<i id="', uuid, '-btn" class="', zcls, '-image-btn"></i>');
         out.push('</div>');
 
     } else {
