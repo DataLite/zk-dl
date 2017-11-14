1b0acefd6562e38b493824ecc65d72cc33e5d4fc
Martin Peterka <martin.peterka@datalite.cz>
2017-11-14 11:16:40 +0100

    SPM-1565 Profily - Veřejný profil - nejde zaškrtnout

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
+++ ZKDLComponents/src/main/resources/web/js/dlzklib/mold/dllabeledtag.js
@@ -1,6 +1,6 @@
 (function (out) {
     var uuid = this.uuid;
-    out.push('<div draggable="true" droppable="true" ', this.domAttrs_(), ' id="', uuid , '-root">');
+    out.push('<div ', this.domAttrs_(), ' id="', uuid , '-root">');
     out.push('<span class="dl-labeled-tag-label" id="', uuid, '-label">', this.getLabel(), '</span>');
     out.push('<span class="dl-labeled-tag-value" id="', uuid, '-value">', this.getValue(), '</span>');
     out.push('<span class="z-icon-times" id="', uuid, '-close"></span>');
