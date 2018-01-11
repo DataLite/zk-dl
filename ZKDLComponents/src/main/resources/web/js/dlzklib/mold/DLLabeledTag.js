4b483f1edec3abbb5a6f14d54c4ba3ea7383f03b
Jan Tecl <tecl@datalite.cz>
2018-01-11 22:07:20 +0100

    SEC-312,SEC-404 - Oprava vzhledu dotazníku, úprava ikony dotazníku

--- ZKDLComponents/src/main/resources/web/js/dlzklib/mold/DLLabeledTag.js
+++
@@ -1,8 +0,0 @@
-(function (out) {
-    var uuid = this.uuid;
-    out.push('<div ', this.domAttrs_(), ' id="', uuid , '-root">');
-    out.push('<span class="dl-labeled-tag-label" id="', uuid, '-label">', this.getLabel(), '</span>');
-    out.push('<span class="dl-labeled-tag-value" id="', uuid, '-value">', this.getValue(), '</span>');
-    out.push('<span class="z-icon-times" id="', uuid, '-close"></span>');
-    out.push('</div>');
-});
